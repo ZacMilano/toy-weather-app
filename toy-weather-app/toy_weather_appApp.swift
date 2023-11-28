eaaffe1d7166a40571fd1073330a88097d81754e
zacmonroe <zmonroe0820@gmail.com>
2023-11-27 18:18:05 -0600

    Initial Commit

---
+++ toy-weather-app/toy_weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  toy_weather_appApp.swift
+//  toy-weather-app
+//
+//  Created by Zac Milano on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct toy_weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
